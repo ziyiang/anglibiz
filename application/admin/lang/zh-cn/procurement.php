2c425f7ea3bb6a8cbe3d00dfdbdd773a7ee84f8f
ziyi_ang@126.com <ziyi_ang@126.com>
2018-10-21 17:35:08 +0800

    创建采购模块，导入功能

---
+++ application/admin/lang/zh-cn/procurement.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'Id'  =>  '主键',
+    'Atuhor_id'  =>  '上传人',
+    'Purchase_date'  =>  '销售日期',
+    'Sales_person'  =>  '销售人',
+    'Sales_mobile'  =>  '销售人联系电话',
+    'Factory_name'  =>  '厂名',
+    'Factory_mobile'  =>  '联系电话',
+    'Product_name'  =>  '品名',
+    'Product_number'  =>  '数量',
+    'Cost_unit_price'  =>  '成本单价',
+    'Cost_mount'  =>  '成本金额',
+    'Advance_payment'  =>  '预付款',
+    'Balance_mount'  =>  '余款',
+    'Delivery_date'  =>  '发货日期',
+    'Customer_name'  =>  '客户名称',
+    'Sales_unit_price'  =>  '销售单价',
+    'Sales_amount'  =>  '销售金额',
+    'Rebate_amount'  =>  '回扣金额',
+    'Order_person'  =>  '下单人员',
+    'Remark'  =>  '备注',
+    'Link'  =>  '链接',
+    'Create_time'  =>  '创建时间'
+];
---
+++ public/assets/js/backend/procurement.js
@@ -0,0 +1,73 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'procurement/index',
+                    add_url: 'procurement/add',
+                    edit_url: 'procurement/edit',
+                    del_url: 'procurement/del',
+                    multi_url: 'procurement/multi',
+                    import_url: 'procurement/import',
+                    table: 'procurement',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'atuhor_id', title: __('Atuhor_id')},
+                        {field: 'purchase_date', title: __('Purchase_date'), operate:'RANGE', addclass:'datetimerange'},
+                        {field: 'sales_person', title: __('Sales_person')},
+                        {field: 'sales_mobile', title: __('Sales_mobile')},
+                        {field: 'factory_name', title: __('Factory_name')},
+                        {field: 'factory_mobile', title: __('Factory_mobile')},
+                        {field: 'product_name', title: __('Product_name')},
+                        {field: 'product_number', title: __('Product_number')},
+                        {field: 'cost_unit_price', title: __('Cost_unit_price'), operate:'BETWEEN'},
+                        {field: 'cost_mount', title: __('Cost_mount'), operate:'BETWEEN'},
+                        {field: 'advance_payment', title: __('Advance_payment'), operate:'BETWEEN'},
+                        {field: 'balance_mount', title: __('Balance_mount'), operate:'BETWEEN'},
+                        {field: 'delivery_date', title: __('Delivery_date'), operate:'RANGE', addclass:'datetimerange'},
+                        {field: 'customer_name', title: __('Customer_name')},
+                        {field: 'sales_unit_price', title: __('Sales_unit_price'), operate:'BETWEEN'},
+                        {field: 'sales_amount', title: __('Sales_amount'), operate:'BETWEEN'},
+                        {field: 'rebate_amount', title: __('Rebate_amount'), operate:'BETWEEN'},
+                        {field: 'order_person', title: __('Order_person')},
+                        {field: 'remark', title: __('Remark')},
+                        {field: 'link', title: __('Link')},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange'},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
